74e7a498526a7152df5834bbdcad6827114e45ab
Olivia Phillips <op79@sussex.ac.uk>
2017-04-07 11:12:49 +0100

    olivias-workload Filled in client and stock methods

--- src/HardStock.java
+++ src/HardStock.java
@@ -1,12 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cao31
+ */
 public class HardStock extends StockType{
     
+   public int variation;
+  public int valueOfWorth;
+ 
   public HardStock() {
-    }
+    variation = 0;
+    valueOfWorth = 0;
+  }
     
-  void getValueOfWorth(){    
-    }
+   @Override
+  int getValueOfWorth(){
+    return valueOfWorth;
+  }
+
+  int getVariation(){
+    return variation;
+  }
   
-  void setValueOfWoth(){
+   @Override
+  void setValueOfWorth(int val){
+    valueOfWorth = val;
+  }
+
+  void setVariation(int var){
+    variation = var;
   }
     
 }
--- src/TechnologyStock.java
+++ src/TechnologyStock.java
@@ -1,12 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cao31
+ */
 public class TechnologyStock extends StockType {
     
-    public TechnologyStock() {
-    }
+  public int variation;
+  public int valueOfWorth;
+ 
+  public TechnologyStock() {
+    variation = 0;
+    valueOfWorth = 0;
+  }
     
-  void getValueOfWorth(){    
-    }
+  @Override
+  int getValueOfWorth(){
+    return valueOfWorth;
+  }
+
+  int getVariation(){
+    return variation;
+  }
   
-  void setValueOfWoth(){
+  @Override
+  void setValueOfWorth(int val){
+    valueOfWorth = val;
+  }
+
+  void setVariation(int var){
+    variation = var;
   }
     
 }
